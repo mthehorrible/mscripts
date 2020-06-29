477e5a48af78bf02f6773960f181c9e8d490ca7d
mthehorrible <mthehorrible@pm.me>
2020-06-28 20:28:03 -0400

    changed address

--- grab-gits.sh
+++ grab-gits.sh
@@ -4,4 +4,4 @@ mkdir ~/apps
 mkdir ~/apps/git
 cd ~/apps/git
 git clone https://github.com/mthehorrible/mscripts.git
-git clone https://github.com/mthehorrible/dotfiles.git
+git clone git@github.com:mthehorrible/dotfiles.git
